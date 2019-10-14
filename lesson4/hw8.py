345d563a345dc21e414a5f7ecd362547b0c8bd77
IhorRe <ihorremen@gmail.com>
2019-10-14 17:46:08 +0300

    Lesson2

--- lesson4/hw8.py
+++ lesson4/hw8.py
@@ -1,22 +1,44 @@
+def count1(text):
+    count1=0
+    while True:
+        s = text
+        for i in s:
+            if i in '0123456789':
+                count1 += 1
+        return 'В тексте {} цыфр'.format(count1)
+
 def text_stat(text):
+    z = text.lower( )
     print("статистика символов ----->")
     l = []
-    s = ''.join(list(text))
+    s = ''.join(list(z))
     for i in s:
-        if i not in l and i.isalpha() or i.isascii():
-            print(i + ': ' + str(s.count(i)))
-            l.append(i)
+        if i not in l and i.isprintable():
+            l+=i
+            print("'"+i+"'" + ': ' + str(s.count(i)))
     print("<-----Статистика символов")
     print("*"*50)
     print("статистика слов ---->")
-    words1 = text
+    words1 = z
     words1 = words1.split()
     for i in words1:
         if i not in l and i.isascii() or i.isalpha():
-            print(i + ': ' + str(words1.count(i)))
-            l.append(i)
-    print ('<----- статистика слов')
+            print("'"+i+"'" + ': ' + str(words1.count(i)))
+    print('<----- статистика слов')
 
-
-text = input('текст для экспериментов: ')
+print('текст для экспериментов:  ')
+text = ""
+count= 0
+stopword = ""
+while True:
+    line = input( )
+    if line.strip() == stopword:
+        break
+    count+=1
+    text += "%s\n" % line
+print("<-----------Исходный текс: ")
+print(text)
+print("Исходный текс-------------> ")
+print(count1(text))
+print("Количество строк в тексте:  ", count)
 print(text_stat(text))
